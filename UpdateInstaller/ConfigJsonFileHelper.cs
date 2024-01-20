e6ad5837634dfacf070279692713dd23e9ffc676
na1307 <na1307@outlook.kr>
2024-01-20 14:06:13 +0900

    null 처리

--- UpdateInstaller/ConfigJsonFileHelper.cs
+++ UpdateInstaller/ConfigJsonFileHelper.cs
@@ -5,7 +5,7 @@ namespace UpdateInstaller;
 public static class ConfigJsonFileHelper {
     private static readonly JObject jobj = JObject.Parse(File.ReadAllText(ConfigFileName));
     private static readonly UpdatePath[] updatePaths = jobj["UpdatePaths"]!.ToObject<UpdatePath[]>()!;
-    private static readonly PreUpdate[] preUpdates = jobj["PreUpdates"]!.ToObject<PreUpdate[]>()!;
+    private static readonly PreUpdate[]? preUpdates = jobj["PreUpdates"]!.ToObject<PreUpdate[]>();
 
     public static string? OSVersion { get; } = getConfigValue(nameof(OSVersion));
     public static string? SPVersion { get; } = getConfigValue(nameof(SPVersion));
@@ -15,7 +15,7 @@ public static class ConfigJsonFileHelper {
     public static string? PreUpdatePath { get; } = getConfigValue(nameof(PreUpdatePath));
 
     public static UpdatePath? GetUpdatePath(int index) => updatePaths.ElementAtOrDefault(index - 1);
-    public static PreUpdate? GetPreUpdate(int index) => preUpdates.ElementAtOrDefault(index - 1);
+    public static PreUpdate? GetPreUpdate(int index) => preUpdates?.ElementAtOrDefault(index - 1);
 
     private static string? getConfigValue(string path) => jobj[path]?.ToString();
 }
