020af62ee682fff14dae02ff06adcc70970a4689
na1307 <na1307@outlook.kr>
2024-01-22 00:02:48 +0900

    상수

--- UpdateInstaller/Constants.cs
+++ UpdateInstaller/Constants.cs
@@ -4,6 +4,6 @@ public static class Constants {
     public const ulong BuildNumber = 66;
     public const string ConfigFileName = "UpdateInstaller.json";
     public const int WM_QUERYENDSESSION = 0x11;
-    public static readonly CpuArch Arch = (CpuArch)Enum.Parse(typeof(CpuArch), IntPtr.Size == 8 ? "x64" : "x86"); // 32비트 또는 64비트 감지
+    public static readonly CpuArch Arch = IntPtr.Size == 8 ? CpuArch.x64 : CpuArch.x86; // 32비트 또는 64비트 감지
     public static readonly OSPlatform Platform = !Winver.IsWindowsServer ? OSPlatform.Client : OSPlatform.Server;
 }
